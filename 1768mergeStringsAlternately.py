c4a7a588c5e0a3f448637a6e2bc981a624177577
Malhar Thanki <67618617+malhaaar@users.noreply.github.com>
2024-05-27 21:36:48 +0530

    Update 1768mergeStringsAlternately.py

--- 1768mergeStringsAlternately.py
+++ 1768mergeStringsAlternately.py
@@ -1,3 +1,4 @@
+# solution1
 n1,n2 = len(word1), len(word2)
 n12 = ""
 if n1 == n2:
@@ -22,3 +23,15 @@ else:
         n12+=word2[i]
 
 return(n12)
+
+# solution2
+
+s = ""
+n = min(len(word1),len(word2))
+for i in range(n):
+    s = s + word1[i] + word2[i]
+if len(word1) > len(word2):
+    s = s + word1[n:]
+else:
+    s = s + word2[n:]
+return s 
